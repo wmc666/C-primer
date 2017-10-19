2c1cda33773091a7f2cd8954bd290e4d22e8d01f
wmc666 <73300@163.com>
2017-10-19 21:11:55 +0800

    primer_3

---
+++ chapter2/2.18.cpp
@@ -0,0 +1,9 @@
+//编写代码分别更改指针的值以及指针所指对象的值 
+#include<iostream>
+using namespace std;
+int main()
+{
+    int null=0,*p=nullptr;
+	cout<<p; 
+	return 0;
+} 
---
+++ chapter2/2.8.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=20;
+	int *p=&a;
+	cout<<*p<<endl;
+	return 0;
+}
